f194675c144e55fa1e39e29ce7b10c2f351ab95b
Vivek-venmanassery-sunilkumar <153194771+Vivek-venmanassery-sunilkumar@users.noreply.github.com>
2024-01-01 19:30:28 +0530

    Blackjack simple.py

--- Blackjack simple.py
+++ Blackjack simple.py
@@ -28,12 +28,34 @@ def draw_cards1():
         draw_cards = {}
         draw_cards["user"] = [random.choice(cards), random.choice(cards)]
         draw_cards["handler"] = [random.choice(cards), random.choice(cards)]
+        user_sum = sum(draw_cards["user"])
+        dealer_sum = sum(draw_cards["handler"])
+        if draw_cards["user"][0] == 11 and draw_cards["user"][1] == 11:
+            draw_cards["user"][0] = 1
+        elif (draw_cards["user"][0] == 11 or draw_cards["user"][1] == 11) and user_sum > 21:
+            draw_cards["user"][0] = 1 if draw_cards["user"][0] == 11 else draw_cards["user"][0]
+            draw_cards["user"][1] = 1 if draw_cards["user"][1] == 11 else draw_cards["user"][1]
+        if draw_cards["handler"][0] == 11 and draw_cards["handler"][1] == 11:
+            draw_cards["handler"][1] = 1
+        elif (draw_cards["handler"][0] == 11 or draw_cards["handler"][1] == 11) and dealer_sum > 21:
+            draw_cards["handler"][0] = 1 if draw_cards["handler"][0] == 11 else draw_cards["handler"][0]
+            draw_cards["handler"][1] = 1 if draw_cards["handler"][1] == 11 else draw_cards["handler"][1]
         print(f"your hand is {draw_cards['user']}.")
         print(f"dealers hand is {draw_cards['handler'][0]}")
         direction = input("if you want to draw type 'd' and to hold type 'h'.")
         if direction == "d":
             draw_cards["user"].append(random.choice(cards))
-            draw_cards["handler"].append(random.choice(cards))
+            draw_cards["handler"].append(random.choice(cards))'
+            user_sum1 = sum(draw_cards["user"])
+            dealer_sum1 = sum(draw_cards["handler"])
+            if draw_cards["user"][2] == 11 and user_sum1 > 21:
+                draw_cards["user"][2] = 1
+            else:
+                draw_cards["user"][2]
+            if draw_cards["handler"][2] == 11 and dealer_sum1 > 21:
+                draw_cards["handler"][2] = 1
+            else:
+                draw_cards["handler"][2]
             print(f"Your hand is {draw_cards['user']}.")
             print(f"Dealers hand is {draw_cards['handler']}")
             check(sum(draw_cards["user"]), sum(draw_cards["handler"]))
